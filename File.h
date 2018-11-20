0ca3f839b8da6ddf6f5e29241a273a5a7d05f8e1
Gabriel Coelho <gabriellc2307@gmail.com>
2018-11-20 21:17:46 -0200

    Create File.h

---
+++ File.h
@@ -0,0 +1,17 @@
+#ifndef TESTES_H
+#define TESTES_H
+
+void colorirQuadradoCor(int i);
+void drawQuadradoCor();
+void drawTriangleMesh(char file_name[]);
+void draw();
+void display(void);
+void init(void);
+void visualization(void);
+void reshape(GLsizei w, GLsizei h);
+void mouse(int button, int state, int x, int y);
+void keyboard (unsigned char key, int x, int y);
+void specialkey (int key, int x, int y);
+
+
+#endif
