754e2123018bd90b40320f726bc51f2248db9df2
Gabriel Coelho <gabriellc2307@gmail.com>
2018-11-21 00:38:38 -0200

    Update File.h

--- File.h
+++ File.h
@@ -1,6 +1,14 @@
 #ifndef TESTES_H
 #define TESTES_H
 
+typedef struct {
+  double x, y, z;
+} vertice;
+
+typedef struct {
+  int a, b, c;
+} face;
+
 void colorirQuadradoCor(int i);
 void drawQuadradoCor();
 void drawTriangleMesh(char file_name[]);
@@ -12,6 +20,12 @@ void reshape(GLsizei w, GLsizei h);
 void mouse(int button, int state, int x, int y);
 void keyboard (unsigned char key, int x, int y);
 void specialkey (int key, int x, int y);
+
+vertice getVertice(char str[]);
+face getFace(char str[]);
+void limpar();
+void escreverArquivo();
+void lerArquivo();
 
 
 #endif
